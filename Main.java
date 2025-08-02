23f298615805354449290234409a369b2a0b8b00
Alexey-Kn1 <knysh.aleksey@yandex.ru>
2025-08-02 17:03:04 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    private static final int MEMBERS_NUMBER = 10;
+
+    public static void main(String[] args) throws IOException {
+        int[][] teams = {
+                { 45, 31, 24, 22, 20, 17, 14, 13, 12, 10 },
+                { 31, 18, 15, 12, 10, 8, 6, 4, 2, 1 },
+                { 51, 30, 10, 9, 8, 7, 6, 5, 2, 1 }
+        };
+
+        int[] nationalTeam = mergeAll(teams);
+        System.out.println(Arrays.toString(nationalTeam)); // [51, 45, 31, 31, 30, 24, 22, 20, 18, 17]
+    }
+
+    /** Метод для слияния всех команд в одну национальную */
+    public static int[] mergeAll(int[][] teams) {
+        int[] res = teams[0];
+
+        for (int i = 1; i < teams.length; i++) {
+            merge(res, teams[i]);
+        }
+
+        return res;
+    }
+
+    /** Метод для слияния двух команд в одну */
+    public static int[] merge(int[] teamA, int[] teamB) {
+        int iA = 0;
+        int iB = 0;
+
+        while (iA < MEMBERS_NUMBER) {
+            if (iB < teamB.length && teamB[iB] > teamA[iA]) {
+                teamA[iA] = teamB[iB];
+
+                iB++;
+            }
+
+            iA++;
+        }
+
+        return teamA;
+    }
+}
